b48556d1d286213da04fb10fabb0ab464781f66f
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-01-05 15:02:48 -0500

    added components and their styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+
+
 import React from 'react'
 
-function index() {
+function HomePage() {
   return (
-    <div>index</div>
+    <div>Home Page</div>
   )
 }
 
-export default index
+export default HomePage
