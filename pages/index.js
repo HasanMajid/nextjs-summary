bebf7f96e0114dfb3e501b2883b54cb4da34af1e
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-01-05 14:44:09 -0500

    added next link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>News Page</h1>;
-
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news"> News 1</Link>
+        </li>
+        <li>News 2</li>
+        <li>News 3</li>
+      </ul>
+    </Fragment>
+  );
 }
