0f0d3d3d4f0f6a32976e3200a3817d8acef9fa31
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-01-05 19:26:13 -0500

    imported MeetupDetail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function Meetup() {
   return (
-    <div>Meetup Page</div>
-  )
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png"
+      title="First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+    />
+  );
 }
 
-export default Meetup
+export default Meetup;
